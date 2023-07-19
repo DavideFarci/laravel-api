de39fb3a909d51dd2c90ed775588882072f6500a
DavideFarci <yank92.df@gmail.com>
2023-07-19 11:43:06 +0200

    add Lead controller, model and his route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\TypeController;
 use App\Http\Controllers\Api\ProjectController;
 
@@ -23,3 +24,5 @@ use App\Http\Controllers\Api\ProjectController;
 Route::get('projects', [ProjectController::class, 'index'])->name('api.projects.index');
 Route::get('projects/{project}', [ProjectController::class, 'show'])->name('api.projects.show');
 Route::get('types', [TypeController::class, 'index'])->name('api.types.show');
+
+Route::post('leads/', [LeadController::class, 'store'])->name('api.leads.store');
